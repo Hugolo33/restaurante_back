0dfafbb03d7981cff85ad6c9c78a6f6da6b971d9
Hugo Guadalajara Lopez <guadalajarahugo@gmail.com>
2023-12-04 16:32:50 +0100

    Creo toda la parte de las reviews

---
+++ src/controllers/review.controller.js
@@ -0,0 +1,34 @@
+const ReviewModel = require('../models/review.model');
+
+const getAll = async (req, res) => {
+    try {
+        const [result] = await ReviewModel.selectAll();
+        res.json(result);
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+};
+
+// const getById = async (req, res) => {
+//     try {
+//         const { userId } = req.params;
+//         const [result] = await ReviewModel.selectById(userId);
+//         res.json(result);
+//     } catch (error) {
+//         res.json({ error: error.message });
+//     }
+// };
+
+const create = async (req, res) => {
+    try {
+        const [result] = await ReviewModel.insert(req.body);
+        const { insertId } = result
+        const [newReview] = await ReviewModel.selectById(insertId)
+        res.json(newReview[0]);
+
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+};
+
+module.exports = { getAll, create };
---
+++ src/routes/api/reviews.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+
+const reviewController = require('../../controllers/review.controller');
+
+router.get('/', reviewController.getAll);
+
+router.post('/', reviewController.create);
+
+
+module.exports = router
