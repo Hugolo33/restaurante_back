82812897c68355d7c86d930bd9e6dbb1fb998afb
Rafael Sánchez-Piñol <sanchez.rafael19@gmail.com>
2023-12-04 15:58:58 +0100

    metodos del menu hechos, falta middleware (check role)

---
+++ src/controllers/menu.controller.js
@@ -0,0 +1,49 @@
+const menuModel = require('../models/menu.model');
+
+
+const getAllMenus = async (req, res) => {
+    // si la peticion atraviesa con extio el metodo checkToken, puedo acceder al valor de req.user
+
+    // console.log('FINAL', req.user);
+
+    //Solo puede acceder el admin
+
+    const [result] = await menuModel.selectAll();
+
+    res.json(result);
+}
+
+const getMenuById = async (req, res) => {
+    try {
+        const menuId = req.params.menuId;
+
+        const [result] = await menuModel.selectId(menuId);
+
+        if (result.length === 0) return res.json({ fatal: 'El id del menu no existe' });
+
+
+        res.json(result[0]);
+
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+}
+const createMenu = async (req, res) => {
+    try {
+        const [result] = await menuModel.insert(req.body);
+
+
+        const [menu] = await (menuModel.selectId(result.insertId));
+        res.json(menu[0]);
+
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+}
+
+
+module.exports = {
+    getAllMenus,
+    getMenuById,
+    createMenu
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 
-router.use('/users', require('./api/users'))
-router.use('/reservations', require('./api/reservations'))
+// router.use('/users', require('./api/users'))
+// router.use('/reservations', require('./api/reservations'))
 router.use('/menu', require('./api/menu'))
-router.use('/reviews', require('./api/reviews'))
+// router.use('/reviews', require('./api/reviews'))
 
 module.exports = router
