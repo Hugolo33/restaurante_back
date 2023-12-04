7498aed5af07def98b9719678b1da980c967aa49
Jacobo Varela <jacobovarelab@gmail.com>
2023-12-04 16:59:03 +0100

    Solo faltan 2 métodos

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
 
-router.use('/users', require('./api/users'))
+// router.use('/users', require('./api/users'))
+
+// esta es la mía
 router.use('/reservations', require('./api/reservations'))
-router.use('/menu', require('./api/menu'))
-router.use('/reviews', require('./api/reviews'))
+
+
+// router.use('/menu', require('./api/menu'))
+// router.use('/reviews', require('./api/reviews'))
 
 module.exports = router
