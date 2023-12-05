f40fa2907dc7c807e1fccdaac8a02d1d0ad30c8b
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-12-05 14:05:15 +0100

    añadidos middlewares para acceder a rutas en menu y reviews

--- src/routes/api/menu.js
+++ src/routes/api/menu.js
@@ -3,12 +3,10 @@ const router = require('express').Router();
 const menuController = require('../../controllers/menu.controller');
 
 const { checkRole } = require("../../helpers/middlewares.js")
-// POST api/menu
 
-// - Crea un nuevo menú menú de ese día
 
-router.get('/', checkRole, menuController.getAllMenus);
+router.get('/', checkRole('admin'), menuController.getAllMenus);
 router.get('/:menuId', menuController.getMenuById)
-router.post('/', checkRole, menuController.createMenu);
+router.post('/', checkRole('admin'), menuController.createMenu);
 
 module.exports = router;
--- src/routes/api/reviews.js
+++ src/routes/api/reviews.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 
 const reviewController = require('../../controllers/review.controller');
+const { checkToken } = require('../../helpers/middlewares');
 
 router.get('/', reviewController.getAll);
 
-router.post('/', reviewController.create);
+router.post('/', checkToken, reviewController.create);
 
 
 module.exports = router
