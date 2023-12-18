6a6a7bb02a029264a21f44fb91af083396b8d3e9
Jacobo Varela <jacobovarelab@gmail.com>
2023-12-18 09:45:05 +0100

    Metidos los middleware

--- src/routes/api/menu.js
+++ src/routes/api/menu.js
@@ -11,7 +11,7 @@ router.get('/latest', menuController.getLatest);
 router.get('/date/:menuDate', menuController.getByDate);
 router.get('/:menuId', menuController.getMenuById);
 router.post('/', checkToken, checkRole('admin'), menuController.create);
-router.put("/:menuId", menuController.updateMenu)
-router.delete("/:menuId", menuController.deleteMenu)
+router.put("/:menuId", checkToken, checkRole('admin'), menuController.updateMenu)
+router.delete("/:menuId", checkToken, checkRole('admin'), menuController.deleteMenu)
 
 module.exports = router;
